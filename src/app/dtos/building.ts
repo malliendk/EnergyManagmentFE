0fb5dd1ed0c3ada2dfe6a9262f4821ec8e6526a4
DMALLIEN <malliendk@gmail.com>
2025-01-21 21:55:04 +0100

    Building card views

---
+++ src/app/dtos/building.ts
@@ -0,0 +1,21 @@
+export interface Building {
+
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  color: string;
+  imageUri: string;
+  category: string;
+  gridLoad: number;
+  gridCapacity: number;
+  energyProduction: number;
+  energyConsumption: number;
+  solarPanelAmount: number
+  solarPanelCapacity: number;
+  households: number;
+  popularityIncome: number;
+  goldIncome: number;
+  researchIncome: number;
+  environmentalIncome: number;
+}
