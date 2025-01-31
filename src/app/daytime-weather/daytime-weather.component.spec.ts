5d9541a74f2c2a0ce51ecbc3b67e14ea50813d09
DMALLIEN <malliendk@gmail.com>
2025-01-31 10:29:05 +0100

    removed routing from app component, installed ngBootstrap

---
+++ src/app/daytime-weather/daytime-weather.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DaytimeWeatherComponent } from './daytime-weather.component';
+
+describe('DaytimeWeatherComponent', () => {
+  let component: DaytimeWeatherComponent;
+  let fixture: ComponentFixture<DaytimeWeatherComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DaytimeWeatherComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DaytimeWeatherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/daytime-weather/daytime-weather.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BehaviorSubject, Observable, Subscription} from "rxjs";
+import {ClockService} from "../services/clock.service";
+
+@Component({
+  selector: 'app-daytime-weather',
+  templateUrl: './daytime-weather.component.html',
+  styleUrls: ['./daytime-weather.component.css']
+})
+export class DaytimeWeatherComponent implements OnInit, OnDestroy{
+  private timer: any;
+  private timeSubject = new BehaviorSubject<string>('00:00');
+
+  ngOnInit() {
+    this.startTimer();
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  getTime(): Observable<string> {
+    return this.timeSubject.asObservable();
+  }
+
+  startTimer(): void {
+    let hours = 0;
+    let minutes = 0;
+
+    this.timer = setInterval(() => {
+      minutes += 5;
+      if (minutes >= 60) {
+        hours = (hours + Math.floor(minutes / 60)) % 24;
+        minutes = minutes % 60;
+      }
+
+      const formattedHours = hours.toString().padStart(2, '0');
+      const formattedMinutes = minutes.toString().padStart(2, '0');
+      this.timeSubject.next(`${formattedHours}:${formattedMinutes}`);
+    }, 1000);
+  }
+
+  stopTimer(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+}
+
