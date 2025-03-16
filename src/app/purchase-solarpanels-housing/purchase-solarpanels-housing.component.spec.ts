3a1c38dff6af33f5ffbd14c276a892a57a23d164
DMALLIEN <malliendk@gmail.com>
2025-03-16 23:02:05 +0100

    day-weather fixed and starting events

---
+++ src/app/purchase-solarpanels-housing/purchase-solarpanels-housing.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PurchaseSolarpanelsHousingComponent } from './purchase-solarpanels-housing.component';
+
+describe('PurchaseSolarpanelsHousingComponent', () => {
+  let component: PurchaseSolarpanelsHousingComponent;
+  let fixture: ComponentFixture<PurchaseSolarpanelsHousingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PurchaseSolarpanelsHousingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PurchaseSolarpanelsHousingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/purchase-solarpanels-housing/purchase-solarpanels-housing.component.ts
@@ -0,0 +1,58 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {SOLAR_PANEL_PRICE} from "../constants";
+import {CurrencyPipe} from "@angular/common";
+import {Building} from "../dtos/building";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+    selector: 'app-purchase-solarpanels-housing',
+  imports: [
+    CurrencyPipe,
+    FormsModule
+  ],
+    templateUrl: './purchase-solarpanels-housing.component.html',
+    standalone: true,
+    styleUrl: './purchase-solarpanels-housing.component.css'
+})
+export class PurchaseSolarpanelsHousingComponent implements OnInit {
+
+  @Input() building!: Building | null;
+  @Output() passSolarPanelPurchase = new EventEmitter<{
+    amountOfSolarPanels: number,
+    totalCost: number
+  }>();
+
+  solarPanelSetAmount: number = 0;
+  totalCostSolarPanels: number = 0;
+
+  maxSolarSets!: number;
+  minSolarPanelSets: number = 0;
+
+  ngOnInit() {
+    this.maxSolarSets = this.building!.solarPanelCapacity - this.building!.solarPanelAmount;
+  }
+
+  calculateTotalSolarPanelPrice(numberOfSolarSets: HTMLInputElement) {
+    if (numberOfSolarSets.valueAsNumber > this.maxSolarSets) {
+      numberOfSolarSets.value = this.maxSolarSets.toString();
+    } else if (numberOfSolarSets.valueAsNumber < this.minSolarPanelSets) {
+      numberOfSolarSets.value = this.minSolarPanelSets.toString();
+    }
+    numberOfSolarSets.valueAsNumber ?
+      this.totalCostSolarPanels = SOLAR_PANEL_PRICE.price * numberOfSolarSets.valueAsNumber :
+      this.totalCostSolarPanels = 0;
+  }
+
+  preventNegativeInput(event: KeyboardEvent) {
+    if (event.key === '-') {
+      event.preventDefault();
+    }
+  }
+
+  purchaseSolarPanels(numberOfSets: number, totalCost: number) {
+    this.passSolarPanelPurchase.emit({
+      amountOfSolarPanels: numberOfSets,
+      totalCost: totalCost
+    });
+  }
+}
